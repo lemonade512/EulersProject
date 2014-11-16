ab07788cc3a1361e44c0514ab61baf3a90f25893
lemonade512 <philliplemons@utexas.edu>
2014-11-16 01:40:36 -0600

    Added problem 3

---
+++ Problem3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from math import sqrt
+
+# TODO check problem forums to make sure all of this is implemented correctly
+
+def is_prime(num):
+    for i in xrange(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+def prime_factors(num):
+    factors = []
+    for i in xrange(2, int(sqrt(num)) + 1):
+        if is_prime(i) and num % i == 0:
+            factors.append(i)
+    return factors
+
+if __name__ == "__main__":
+    answer = prime_factors(600851475143)[-1]
+    print answer
